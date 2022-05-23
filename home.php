c61215a786fa7691e3c2e5ae1e2e89ca4a2fdc0a
GilbertSEKANYANA <101249520+GilbertSEKANYANA@users.noreply.github.com>
2022-05-23 21:10:21 +0200

    Add files via upload

---
+++ home.php
@@ -0,0 +1,68 @@
+
+<br><br>
+
+
+<head>
+<script src="js/jquery-1.10.2.js"></script>
+<script src="js/jquery-ui.js"></script>
+<link type="text/css" href="css/jquery-ui.css" rel="stylesheet" />
+<script type="text/javascript" src="js/facebox.js"></script>
+<link type="text/css" href="css/facebox.css" rel="stylesheet" />
+<script>
+  $(function() {
+    $( "#tabs" ).tabs();
+	$('a[rel*=facebox]').facebox();
+	$( ".datepicker" ).datepicker();
+  });
+  
+  $(document).ready(function(){
+	// Write on keyup event of keyword input element
+	$("#searchme").keyup(function(){
+		// When value of the input is not blank
+		if( $(this).val() != "")
+		{
+			// Show only matching TR, hide rest of them
+			$("#searchTbl tbody>tr").hide();
+			$("#searchTbl td:contains-ci('" + $(this).val() + "')").parent("tr").show();
+		}
+		else
+		{
+			// When there is no input or clean again, show everything back
+			$("#searchTbl tbody>tr").show();
+		}
+	});
+});
+// jQuery expression for case-insensitive filter
+$.extend($.expr[":"], 
+{
+    "contains-ci": function(elem, i, match, array) 
+	{
+		return (elem.textContent || elem.innerText || $(elem).text() || "").toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
+	}
+});
+
+
+  </script>
+ <style>
+table tr:nth-child(even) { /*(even) or (2n 0)*/
+	background: #A4D1FF;
+}
+table tr:nth-child(odd) { /*(odd) or (2n 1)*/
+	background: #EAF4FF;
+}
+</style>
+</head>
+<html>
+<body>
+<center> <h2>WELCOME TO SUPERNOVA TECHNOLOGY GROUP Ltd </h2> </center>
+<div id="tabs">
+  <ul>
+    <li><a href="adminlogin.php"> Admin</a></li>
+    <li><a href="userlogin.php">User</a></li>
+
+  </ul>
+  
+</div>
+
+</body>
+</html>
