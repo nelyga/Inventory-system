33f112dc655cca7692147a63cd52e196462a86fa
GilbertSEKANYANA <101249520+GilbertSEKANYANA@users.noreply.github.com>
2022-05-23 21:44:53 +0200

    userlogn

--- userlogin.php
+++ userlogin.php
@@ -4,9 +4,9 @@ session_start();
 if(isset($_POST['login'])){
     $email = $_POST['email'];
     $password = $_POST['password'];
-    $sql = 'SELECT * FROM user where email=:email AND password=:pass';
+    $sql = 'SELECT * FROM user where email=:email AND password=:password';
 $statement = $conn->prepare($sql);
-$statement->execute(['email' => $email,'pass' => $password ]);
+$statement->execute(['email' => $email,'password' => $password ]);
 $login = $statement->rowCount();
 if ($login > 0) {
     $_SESSION['user']=$_POST['email'];
@@ -28,7 +28,7 @@ if ($login > 0) {
 </head>
 <body>
     <form method="post" action="userlogin.php">
-        <H2>User Login</H2>
+        <H3>User Login</H3>
       
         Email:<br><input type="email" name="email"><br>
         Password:<br><input type="password" name="password"><br><br>
